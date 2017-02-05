bd648acc2c90e264bb92216abdbe322cc3cd9b48
Edison Lai <lhwedison@gmail.com>
2017-02-05 12:57:19 +0800

    New update

---
+++ Py_Learn/PyLearning/PyLearning.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+
+print("Hello world")
+
+
+principal = 1000
+rate = 0.05
+numyears = 5
+year = 1
+
+while year <= numyears:
+    principal = principal * (1 + rate)
+    print(year, principal)
+    year += 1
+
+
